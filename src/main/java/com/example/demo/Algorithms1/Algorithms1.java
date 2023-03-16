f37d6e49a2dfb13ab2238bc87d3b81be50706001
Arkadiusz Górka <arekgorka91@gmail.com>
2023-03-16 23:54:32 +0100

    add findOdd algorithm

--- src/main/java/com/example/demo/Algorithms1/Algorithms1.java
+++ src/main/java/com/example/demo/Algorithms1/Algorithms1.java
@@ -1,5 +1,7 @@
 package com.example.demo.Algorithms1;
 
+import java.util.Arrays;
+
 public class Algorithms1 {
 
     //obliczanie sumy z liczb podzielnych przez 3 i 5 mniejszej od podanej liczby
@@ -11,5 +13,19 @@ public class Algorithms1 {
             }
         }
         return sum;
+    }
+
+    //Given an array of integers, find the one that appears an odd number of times.
+    //There will always be only one integer that appears an odd number of times.
+    public int findOdd(int[] a) {
+        int odd = 0;
+        for (int e : a) {
+            int count = 0;
+            for (int i : a) {
+                if (i == e) {count++;}
+            }
+            if (count == 1 || count % 2 == 1) {odd = e;}
+        }
+        return odd;
     }
 }
